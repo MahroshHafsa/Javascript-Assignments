57d1392c409b9cb25dc4f5924c1d892c8a3e36cc
Mahrosh Hafsa <mahroshhafsa@gmail.com>
2026-03-01 13:11:08 +0500

    Chapter 3 (Variables for Numbers) Done

---
+++ Chapter 3/app.js
@@ -0,0 +1,9 @@
+// Question 1
+var age = 10;   
+alert("I am  " + age + " years old");
+// Question 2
+ alert("You have visited this site 14 times.")
+//  Question 3
+let birthyear = 2015 
+document.write("My birthyear is " + birthyear + "<br>");
+document.write ("Data type of my declare variable is" + birthyear);
